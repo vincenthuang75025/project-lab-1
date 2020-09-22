254d922146fb6cb42382f8b947786036ee794ecb
Vincent Huang <vincenthuang75025@yahoo.com>
2020-09-21 21:06:00 -0500

    improved O(n^3) -> O(n^2)

--- plot.jl
+++ plot.jl
@@ -3,41 +3,39 @@ N = 31 # number of iterations
 
 # creating grid
 points = Dict()
-for x = -1 * N : N
-    for y = -1 * N : N 
-        points[(x,y)] = -1
-    end
-end
+# for x = -1 * N : N
+#     for y = -1 * N : N 
+#         points[(x,y)] = -1
+#     end
+# end
 
 function get_neighbors(pt) # get lattice neighbors of pt = (x,y)
     return [(pt[1]-1, pt[2]), (pt[1]+1, pt[2]), (pt[1], pt[2]-1), (pt[1], pt[2]+1)]
 end
 
 
-# start with bad O(N^3) sol, good enough for now
 points[(0,0)] = 0
 xs = [[] for i=0:N]
 ys = [[] for i=0:N]
 push!(xs[1], 0)
 push!(ys[1], 0)
 for turns = 1 : N 
-    for (pt, turn) in points 
-        neighbors = get_neighbors(pt)
-        valid = true
-        current = 0
-        for neighbor in neighbors
-            if haskey(points, neighbor)
-                if points[neighbor] == turns - 1
-                    current += 1
-                elseif points[neighbor] > 0
-                    valid = false
+    for ind = 1 : size(xs[turns])[1]
+        pt = (xs[turns][ind], ys[turns][ind])
+        for newpt in get_neighbors(pt)
+            if !(haskey(points, newpt))
+                count = 0
+                for neighbor in get_neighbors(newpt)
+                    if haskey(points, neighbor)
+                        count += 1
+                    end
+                end
+                if count == 1
+                    points[newpt] = turns
+                    push!(xs[turns + 1], newpt[1])
+                    push!(ys[turns + 1], newpt[2])
                 end
             end
-        end
-        if valid && current == 1
-            points[pt] = turns
-            push!(xs[turns + 1], pt[1])
-            push!(ys[turns + 1], pt[2])
         end
     end
 end
