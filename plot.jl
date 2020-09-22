5214442621ad1c4b7a4b353db289cd905a330fee
Vincent Huang <vincenthuang75025@yahoo.com>
2020-09-21 21:08:50 -0500

    replace dict with set

--- plot.jl
+++ plot.jl
@@ -2,36 +2,31 @@ using Plots
 N = 31 # number of iterations
 
 # creating grid
-points = Dict()
-# for x = -1 * N : N
-#     for y = -1 * N : N 
-#         points[(x,y)] = -1
-#     end
-# end
+points = Set()
 
 function get_neighbors(pt) # get lattice neighbors of pt = (x,y)
     return [(pt[1]-1, pt[2]), (pt[1]+1, pt[2]), (pt[1], pt[2]-1), (pt[1], pt[2]+1)]
 end
 
 
-points[(0,0)] = 0
 xs = [[] for i=0:N]
 ys = [[] for i=0:N]
+push!(points, (0,0))
 push!(xs[1], 0)
 push!(ys[1], 0)
 for turns = 1 : N 
     for ind = 1 : size(xs[turns])[1]
         pt = (xs[turns][ind], ys[turns][ind])
         for newpt in get_neighbors(pt)
-            if !(haskey(points, newpt))
+            if !(newpt in points)
                 count = 0
                 for neighbor in get_neighbors(newpt)
-                    if haskey(points, neighbor)
+                    if neighbor in points
                         count += 1
                     end
                 end
                 if count == 1
-                    points[newpt] = turns
+                    push!(points, newpt)
                     push!(xs[turns + 1], newpt[1])
                     push!(ys[turns + 1], newpt[2])
                 end
